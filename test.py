51853ed8ab8980b03ef843a62119918fc30eed60
Kozachenko O <90053232+AlexCheshire@users.noreply.github.com>
2022-12-03 20:47:34 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+import app as tested_app
+import json
+
+class FlaskAppTests(unittest.TestCase):
+
+	def setUp(self):
+		self.app = tested_app.test_client()
+
+	def test_get_hello_endpoint(self):
+		r = self.app.get('/')
+		self.assertEqual(r.status, '200 OK')
+		self.assertEqual(r.data, 'Kozachenko Olesia')
+
+if __name__ =='__main__':
+	import xmlrunner
+	runner = xmlrunner.XMLTestRunner(output='test-reporst')
+	unittest.main(testRunner=runner)
+	unittest.main()
